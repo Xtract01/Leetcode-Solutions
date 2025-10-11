90dd1cb6837fd929cc9792b9c8301f7d2052cdb9
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2025-10-12 00:55:52 +0530

    Time: 419 ms (31.89%) | Memory: 250.3 MB (13.52%) - LeetSync

---
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int n;
+    typedef long long ll;
+    unordered_map<ll,ll>mp;
+    vector<ll> t;
+    ll solve(vector<ll>& nums , int i){
+        if(i>=n) return 0;
+        if(t[i]!=-1) return t[i];
+        ll skip = solve(nums,i+1);
+        int j = lower_bound(begin(nums)+i+1,end(nums),nums[i]+3) - begin(nums);
+        ll take = nums[i]*mp[nums[i]]+solve(nums,j);
+        return t[i] = max(skip,take);
+    }
+    ll maximumTotalDamage(vector<int>& power) {
+        for(int &x: power){
+            mp[x]++;
+        }
+        vector<ll> nums(mp.size());
+        for(auto &it : mp ){
+            nums.push_back(it.first);
+        }
+        sort(nums.begin(),nums.end());
+        n = nums.size();
+        t.assign(n+1,-1);
+        return solve(nums,0);
+    }
+};
