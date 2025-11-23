88f45b00fcda08ecced98921a255a662bbbd7df7
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2025-11-23 15:16:23 +0530

    Time: 53 ms (9.54%) | Memory: 46 MB (18.28%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    
+    int helper(vector<int>& nums ,int n ,int i,int rem , vector<vector<int>> &t){
+        if (i >= n) {
+            if (rem == 0) return 0;
+            return INT_MIN;
+        }
+        if(t[i][rem]!=-1) return t[i][rem];
+        int take = nums[i] + helper(nums,n,i+1,(rem + nums[i]) % 3,t);
+        int notTake = helper(nums,n,i+1,rem , t);
+        return t[i][rem] = max(take,notTake);
+    }
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        int sum = 0;
-        vector<int> remainOne;
-        vector<int> remainTwo;
-        for(int &num : nums){
-            sum+= num;
-            if(num % 3==1) remainOne.push_back(num);
-            else if(num % 3 ==2) remainTwo.push_back(num);
-        }
-        if(sum % 3 ==0 ) return sum;
-        sort(begin(remainOne),end(remainOne));
-        sort(begin(remainTwo),end(remainTwo));
-        int result = 0;
-        int remainder = sum % 3;
-        if(remainder == 1){
-            int remove1 = remainOne.size()>=1 ? remainOne[0] : INT_MAX;
-            int remove2 = remainTwo.size()>=2 ?remainTwo[0] + remainTwo[1] :INT_MAX;
-            result = max(result,sum-min(remove1,remove2));
-        }
-        else {
-            int remove1 = remainTwo.size()>=1 ? remainTwo[0] : INT_MAX;
-            int remove2 = remainOne.size()>=2 ?remainOne[0] + remainOne[1] :INT_MAX;
-            result = max(result,sum-min(remove1,remove2));
-        }
-        return result;
+        vector<vector<int>> t(n,vector<int>(3,-1));
+        return helper(nums,n,0,0,t);
     }
 };
