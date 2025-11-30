4958243704c8df467c3e91063ca44460406430d7
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2025-11-30 17:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (46.07%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> t = triangle;
-        for(int row = n-2 ; row>=0 ; row--){
-            for(int col = 0 ; col<= row ; col++){
-                t[row][col] = t[row][col]+min(t[row+1][col],t[row+1][col+1]);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        for(int j = 0 ;j<n ; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i= n-2 ; i>=0 ;i--){
+            for(int j = i ; j>=0 ; j--){
+                int pathOne = triangle[i][j] + dp[i+1][j];
+                int pathTwo = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(pathOne,pathTwo);
             }
         }
-        return t[0][0];
+        return dp[0][0];
     }
 };
