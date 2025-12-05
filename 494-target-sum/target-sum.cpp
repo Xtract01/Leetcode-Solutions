990655b0c69b088593dd0683510f39a01b5c949f
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2025-12-05 21:20:57 +0530

    Time: 398 ms (26.59%) | Memory: 89.9 MB (5.11%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int f(int ind , int sum , int target , vector<int> &nums){
+    int f(int ind , int sum , int target , vector<int> &nums ,  unordered_map<string,int> &mp){
         if(ind==nums.size()){
             if(sum==target) return 1;
             return 0;
         }
-        int pos = f(ind+1,sum+nums[ind],target,nums);
-        int neg = f(ind+1,sum-nums[ind],target,nums);
-        return pos + neg;
+        string key = to_string(ind) + "_" + to_string(sum);
+        if(mp.find(key)!=mp.end()){
+            return mp[key];
+        }
+        int pos = f(ind+1,sum+nums[ind],target,nums,mp);
+        int neg = f(ind+1,sum-nums[ind],target,nums,mp);
+        return mp[key] = pos + neg;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return f(0,0,target,nums);
+        unordered_map<string,int> mp;
+        return f(0,0,target,nums,mp);
     }
 };
