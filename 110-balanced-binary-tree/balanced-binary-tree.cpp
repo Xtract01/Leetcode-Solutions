6b5036ca9e56d75595cdfad688b7ee03676a7a56
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-02-08 20:27:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (20.68%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,39 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int height(TreeNode* root){
-        if(root == NULL) return 0;
-        return 1 + max(height(root->left),height(root->right));
+        if(!root) return 0;
+        int leftH = height(root->left);
+        int rightH = height(root->right);
+
+        return max(leftH,rightH) + 1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            int heightLeft = 0;
-            int heightRight = 0;
-            q.pop();
-            if(temp->left){
-                heightLeft = height(temp->left);
-                q.push(temp->left);
-            }
-            if(temp->right){
-                heightRight = height(temp->right);
-                q.push(temp->right);
-            }
-            if (abs(heightLeft - heightRight) > 1) return false;
-        }
-        return true;
+        if(!root) return true;
+
+        int left = height(root->left);
+        int right = height(root->right);
+        if(abs(left-right)>1) return false;
+
+        return isBalanced(root->left) && isBalanced(root->right);
     }
 };
