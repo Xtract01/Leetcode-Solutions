e8ff7717585ad8802843684322f3dbd1328af439
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-02-22 12:29:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (44.66%) - LeetSync

--- 899-binary-gap/binary-gap.cpp
+++ 899-binary-gap/binary-gap.cpp
@@ -4,13 +4,11 @@ public:
         int curr = 0;
         int prev = -1;
         int result = 0;
-        while(n>0){
-            if((n&1) > 0){
-                result = (prev!=-1) ? max(result,curr-prev) : result;
+        for(int curr=0;curr<32;curr++){
+            if(((n>>curr) &1) > 0){
+                result = (prev!=-1)?max(result,curr-prev):result;
                 prev = curr;
             }
-            curr++;
-            n>>=1;
         }
         return result;
     }
