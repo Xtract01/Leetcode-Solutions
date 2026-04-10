07b3587726fb390ac79cb3d88a9151524880964b
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-04-10 22:32:43 +0530

    Time: 358 ms (50.70%) | Memory: 8.6 MB (93.27%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>> &board,int row,int col , char c){
-        for(int i =0 ; i <9 ; i++){
-            if(board[i][col]==c) return false;
-            if(board[row][i]==c) return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
+    bool isValid(vector<vector<char>>& board,int i , int j , char d){
+        for(int ind=0 ; ind<9 ; ind++){
+            if(board[ind][j]==d) return false;
+            if(board[i][ind]==d) return false;
+        }
+        int start_i = i/3*3;
+        int start_j = j/3*3;
+        for(int k=0 ; k<3 ; k++ ){
+            for(int l = 0;l<3 ; l++){
+                if(board[start_i+k][start_j+l]==d) return false;
+            }
         }
         return true;
     }
-    bool solve(vector<vector<char>> &board){
-        for(int i =0 ; i< board.size() ; i++){
-            for(int j = 0 ; j < board[0].size();j++){
+    bool solve(vector<vector<char>>& board){
+        for(int i=0 ; i<9 ; i++){
+            for(int j=0 ; j<9 ; j++){
                 if(board[i][j]=='.'){
-                    for(char c='1'; c<='9';c++ ){
-                        if(isValid(board,i,j,c)){
-                            board[i][j] = c;
-                            if(solve(board)==true) return true;
-                            else board[i][j] = '.';
+                    for(int d = '1'; d<='9';d++){
+                        if(isValid(board,i,j,d)){
+                            board[i][j] = d;
+                            if(solve(board)){
+                                return true;
+                            }
+                            board[i][j] = '.';
                         }
                     }
                     return false;
