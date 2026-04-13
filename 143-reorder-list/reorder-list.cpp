6e52c91c67e9d611f9710cda754b947a6f29ff81
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-04-13 21:17:35 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (17.40%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+     ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* last = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return last;
+    }
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* rev = reverseList(slow);
+        ListNode* curr = head;
+        while(rev->next){
+            ListNode *tempCurr = curr->next;
+            curr->next = rev;
+            ListNode *tempRev =rev->next ;
+            rev->next = tempCurr;
+
+            curr = tempCurr;
+            rev = tempRev;
+        }
+    }
+};
