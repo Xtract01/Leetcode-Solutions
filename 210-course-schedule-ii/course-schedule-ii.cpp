03796f509d47b3ebb0b846ed52710cfdfc55c0a0
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-04-20 01:48:03 +0530

    Time: 2 ms (76.18%) | Memory: 18.3 MB (56.49%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> adj;
-        for(vector<int> &vec : prerequisites){
-            int first = vec[0];
-            int sec = vec[1];
-            adj[sec].push_back(first);
+        vector<vector<int>> adj(numCourses);
+        for(auto &e:prerequisites){
+            adj[e[1]].push_back(e[0]);
         }
-        vector<int> in(numCourses,0);
-        vector<int> topo ;
-        for(int i = 0; i< numCourses ; i++){
-            for(auto it: adj[i]) in[it]++;
+        vector<int> indegree(numCourses);
+        for(int i=0 ; i<numCourses;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
         }
         queue<int> q;
-        for(int i = 0 ; i< numCourses;i++){
-            if(in[i]==0) q.push(i);
+        vector<int> topo;
+        for(int i=0 ; i< numCourses;i++){
+            if(indegree[i]==0) q.push(i);
         }
         while(!q.empty()){
             int node = q.front();
             q.pop();
             topo.push_back(node);
             for(auto it:adj[node]){
-                in[it]--;
-                if(in[it]==0) q.push(it);
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
         if(topo.size()==numCourses) return topo;
