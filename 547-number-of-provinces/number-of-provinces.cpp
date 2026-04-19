987d8dc6edc2ceef86a35703abf32eebc384614e
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-04-19 14:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (93.05%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,47 +1,24 @@
-class DisjointSet {
-    vector<int> rank,parent;
-  public:
-    DisjointSet(int n){
-        rank.resize(n+1,0);
-        parent.resize(n+1,0);
-        for(int i=0 ; i<=n ; i++){
-            parent[i] = i;
-        }
-    }
-    int findParent(int node){
-        if(node==parent[node]) return node;
-        return parent[node] = findParent(parent[node]);
-    }
-    void unionByRank(int u , int v){
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-        if(rank[ulp_u] < rank[ulp_v]){
-            parent[ulp_u] = ulp_v;
-        }
-        else if(rank[ulp_v] < rank[ulp_u]){
-            parent[ulp_v] = ulp_u;
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-};
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        DisjointSet dis(n);
-        for(int i = 0 ; i< n ; i++){
-            for(int j = 0 ; j< n ; j++){
-                if(isConnected[i][j]==1) dis.unionByRank(i,j);
+private:
+    void dfs(int node ,vector<vector<int>>& isConnected ,vector<int> &vis ){
+        vis[node] = 1;
+        for (int j = 0; j < isConnected.size(); j++) {
+            if (isConnected[node][j] == 1 && !vis[j]) {
+                dfs(j, isConnected, vis);
             }
         }
-        unordered_set<int> st;
-        for(int i = 0 ;i<n ; i++){
-            st.insert(dis.findParent(i));
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int V = isConnected.size();
+        vector<int>vis(V,0);
+        int ans = 0;
+        for(int i=0;i<V ;i++){
+            if(!vis[i]){
+                ans++;
+                dfs(i,isConnected,vis);
+            }
         }
-        return st.size();
+        return ans;
     }
 };
