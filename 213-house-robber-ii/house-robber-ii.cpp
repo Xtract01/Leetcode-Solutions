93288d99f8054739c805be108b346622698c3521
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-06-15 00:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (18.44%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int solve(vector<int>& temp) {
-        int n = temp.size();
-        if(n==0) return 0;
-        if(n==1) return temp[0];
+    int solve(vector<int>& nums){
+        int n = nums.size();
         vector<int> dp(n,0);
-        dp[0] = temp[0];
-        dp[1] = max(temp[1],dp[0]);
-        for(int i = 2 ; i<n ; i++){
-            int take = temp[i] + dp[i-2];
+        dp[0] = nums[0];
+        for(int i = 1 ; i<n ; i++){
+            int take = nums[i];
+            if(i>=2) take+=dp[i-2];
             int notTake = dp[i-1];
             dp[i] = max(take,notTake);
         }
@@ -16,15 +14,9 @@ public:
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==0) return 0;
         if(n==1) return nums[0];
-        vector<int> temp1 , temp2;
-        for(int i = 0 ; i< n ; i++){
-            if(i!=0) temp1.push_back(nums[i]);
-            if(i!=n-1) temp2.push_back(nums[i]);
-        }
-        int ans1 = solve(temp1);
-        int ans2 = solve(temp2);
-        return max(ans1,ans2);
+        vector<int> nums1(nums.begin(),nums.end()-1);
+        vector<int> nums2(nums.begin()+1,nums.end());
+        return max(solve(nums1),solve(nums2));
     }
 };
