bede463bc185521e9f1d683352d84ba299d624c3
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-06-16 23:39:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.89%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
-    int solve(int i,int j , int m ,int n , vector<vector<int>> &dp){
-        if (i >= m || j >= n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = solve(i+1,j,m,n,dp);
-        int right = solve(i,j+1,m,n,dp);
-        return dp[i][j] = down + right;
+    int solve(int row , int col,  vector<vector<int>> &dp){
+        if(row==0 && col==0) return 1;
+        if(dp[row][col]!=-1) return dp[row][col];
+        int up = 0 , left = 0;
+        if(row>=1) up = solve(row-1,col,dp);
+        if(col>=1) left = solve(row,col-1,dp);
+        return dp[row][col]=up+left;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(0,0,m,n,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0 ; i<n ; i++){
+            dp[0][i] = 1;
+        }
+        for(int i=0 ; i<m ; i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1 ; i<m ; i++){
+            for(int j = 1; j<n ; j++){
+                int up = dp[i-1][j];
+                int down = dp[i][j-1];
+                dp[i][j] = up + down;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
