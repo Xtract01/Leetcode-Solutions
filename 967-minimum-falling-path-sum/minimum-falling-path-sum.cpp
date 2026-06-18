ac94270815bb4247cf7686ffe4241bcece1a3297
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-06-19 00:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (42.81%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int> prev(m,0),curr(m,0);
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int j=0 ; j<n ; j++){
+            dp[0][j] = matrix[0][j];
+        }
+        for(int i=1 ; i<m ; i++){
+            for(int j=0 ; j<n ; j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int leftUp = INT_MAX , rightUp = INT_MAX;
+                if(j>=1) leftUp = matrix[i][j] + dp[i-1][j-1];
+                if(j<n-1) rightUp = matrix[i][j] + dp[i-1][j+1];
 
-        for(int j=0 ; j< m; j++) prev[j] = matrix[0][j];
-        for(int i= 1 ; i<n ; i++){
-            for(int j=0 ; j<m ; j++){
-                int u = matrix[i][j] + prev[j];
-                int ld = matrix[i][j] + (j-1>=0 ? prev[j-1] : 1e8);
-                int rd = matrix[i][j] +(j+1<m ? prev[j+1] : 1e8);
-                curr[j]= min({u,ld,rd});
+                dp[i][j] = min({up,leftUp,rightUp});
             }
-            prev = curr;
         }
-        int mini = 1e8;
-        for(int j = 0 ; j<m ; j++){
-            mini = min(mini,prev[j]);
+        int ans = dp[m-1][0];
+        for(int i=1 ; i<n ; i++){
+            ans = min(ans,dp[m-1][i]);
         }
-        return mini;
+        return ans;
     }
 };
