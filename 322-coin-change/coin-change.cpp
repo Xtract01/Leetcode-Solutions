96e8a5dc2a5aa62ee5423e0fd406eee477a47e4f
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-06-27 00:46:19 +0530

    Time: 29 ms (58.33%) | Memory: 22 MB (47.95%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        vector<int>curr(amount+1,0),prev(amount+1,0);
         for(int target = 0 ; target<=amount ; target++){
-            if(target%coins[0]==0) dp[0][target] = target/coins[0];
-            else dp[0][target] = 1e9;
+            if(target%coins[0]==0) prev[target] = target/coins[0];
+            else prev[target] = 1e9;
         }
         for(int ind=1 ; ind<n ; ind++){
             for(int target = 0 ; target<amount+1 ; target++){
                 int take = INT_MAX;
-                if(target>=coins[ind]) take = 1+dp[ind][target-coins[ind]];
-                int notTake = dp[ind-1][target];
-                dp[ind][target] = min(take,notTake);
+                if(target>=coins[ind]) take = 1+curr[target-coins[ind]];
+                int notTake = prev[target];
+                curr[target] = min(take,notTake);
             }
+            prev = curr;
         }
-        int ans =dp[n-1][amount];
+        int ans =prev[amount];
         return ans==1e9 ? -1 : ans;
     }
 };
