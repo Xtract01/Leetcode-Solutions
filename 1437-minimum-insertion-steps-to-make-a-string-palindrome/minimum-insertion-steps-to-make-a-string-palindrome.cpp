1371ed398f4a937b78020e2c636449da7c07b203
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-06-29 00:41:35 +0530

    Time: 34 ms (53.25%) | Memory: 31.6 MB (37.76%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        int m = s.length();
-        string t = s;
-        reverse(t.begin(),t.end());
-        vector<int> prev(m+1,0),curr(m+1,0);
-        for(int j=0 ; j<=m ; j++) prev[j] = 0;
-        for(int i=1 ; i<=m ; i++){
-            for(int j=1 ; j<=m ; j++){
-                if(s[i-1]==t[j-1])  curr[j]= 1 + prev[j-1];
-                else curr[j]= max(prev[j] , curr[j-1]);
-            }
-            prev=curr;
+        string s1 = s;
+        reverse(s.begin(),s.end());
+        int n=s.length();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=0 ; i<=n ; i++){
+            dp[i][0] = dp[0][i] = 0;
         }
-        return m - prev[m];
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n ; j++){
+                if(s1[i-1]==s[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n - dp[n][n];
     }
 };
