4c3d1f8890d1be3aa63d8f9915c75b521e597cbb
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-07-22 22:16:32 +0530

    Time: 137 ms (74.36%) | Memory: 160.7 MB (37.45%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
+    struct trieNode{
+        trieNode* children[10];
+    };
+    trieNode* getNode(){
+        trieNode* newNode = new trieNode();
+        for(int i=0 ; i<10 ;i++) newNode->children[i] = NULL;
+        return newNode;
+    }
+    void insert(trieNode* root,string s){
+        trieNode* pCrawl = root;
+        for(int i=0 ; i<s.length() ; i++){
+            int idx = s[i] - '0';
+            if(pCrawl->children[idx]==NULL) pCrawl->children[idx] = new trieNode();
+            pCrawl = pCrawl->children[idx];
+        }
+    }
+    int lengthOfPrefix(trieNode* root , string s){
+        trieNode* pCrawl = root;
+        int cnt =0;
+        for(int i=0 ; i<s.length() ; i++){
+            int idx = s[i] - '0';
+            if(pCrawl->children[idx]){
+                pCrawl = pCrawl->children[idx];
+                cnt++;
+            }
+            else break;
+        }
+        return cnt;
+    }
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> st;
-        for(int val: arr1){
-            while(!  st.count(val) && val>0){
-                st.insert(val);
-                val/=10;
-            }
+        trieNode* root = getNode();
+        int n = arr1.size();
+        int m = arr2.size();
+        for(int i=0;i<n ; i++){
+            string s = to_string(arr1[i]);
+            insert(root,s);
         }
-        int result = 0;
-        for(int num : arr2){
-            while(!st.count(num) && num>0){
-                num/=10;
-            }
-            if(num>0){
-                result = max(result , (int)log10(num)+1);
-            }
+        int res  = 0;
+        for(int i=0 ; i<m ; i++){
+            string s = to_string(arr2[i]);
+            res = max(res,lengthOfPrefix(root,s));
         }
-        return result;
+        return res;
     }
 };
