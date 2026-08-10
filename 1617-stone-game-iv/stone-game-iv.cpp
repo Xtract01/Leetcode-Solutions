be25a59d59a51a30090b078e7a6296dc60c09640
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-08-10 19:40:42 +0530

    Time: 43 ms (57.44%) | Memory: 13.9 MB (56.63%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int t[100001];
+    bool solve(int n){
+        if(n==0) return false;
+        if(t[n]!=-1) return t[n] ==1?true:false;
+        for(int i=1 ; i*i <=n ; i++){
+            if(!solve(n-(i*i))) return t[n]=true;
+        }
+        return t[n]=false;
+    }
+    bool winnerSquareGame(int n) {
+        memset(t,-1,sizeof(t));
+        return solve(n);
+    }
+};
