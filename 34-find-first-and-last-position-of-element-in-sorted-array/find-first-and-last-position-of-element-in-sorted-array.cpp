98de998464c893d79c2f32aa3109e957c32da6a1
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-08-13 22:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (54.50%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    int lowerBound(vector<int> &nums,int target){
-        int n = nums.size();
-        int lo=0,hi=n-1;
-        int ans = n;
+    int upperBound(vector<int> &nums, int target){
+        int lo = 0 , hi = nums.size() - 1;
+        int ans = nums.size();
         while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(nums[mid]>=target){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid]>target){
                 ans = mid;
-                hi=mid-1;
+                hi = mid-1;
             }
             else lo = mid+1;
         }
         return ans;
     }
-    int upperBound(vector<int> &nums,int target){
-        int n = nums.size();
-        int lo=0,hi=n-1;
-        int ans = n;
+    int lowerBound(vector<int> &nums, int target){
+        int lo = 0 , hi = nums.size() - 1;
+        int ans = -1;
         while(lo<=hi){
-            int mid = lo+(hi-lo)/2;
-            if(nums[mid]>target){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid]>=target){
                 ans = mid;
-                hi=mid-1;
+                hi = mid-1;
             }
             else lo = mid+1;
         }
         return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
         int lb = lowerBound(nums,target);
-        if(lb==nums.size() || nums[lb]!=target) return {-1,-1};
-        return {lb,upperBound(nums,target)-1};
+        int ub = upperBound(nums,target);
+        if (lb == -1 || nums[lb] != target) {
+            return {-1, -1};
+        }
+        return {lb,ub-1};
     }
 };
