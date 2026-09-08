ea956ae1eb1c40f72dd3f86118fb5717c7314b0d
Anirudh Singh <143269166+Xtract01@users.noreply.github.com>
2026-09-09 01:25:33 +0530

    Time: 4 ms (31.04%) | Memory: 10.2 MB (77.61%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    vector<vector<string>> result;
-    int N;
-    bool isValid(vector<string> &board , int row, int col){
-        for(int i=row-1 ; i>=0 ; i--){
-            if(board[i][col]=='Q') return false;
+    bool isValid(vector<string> &board, int r , int c, int n){
+        for(int i=r-1 ; i>=0 ; i--) {
+            if(board[i][c]=='Q') return false;
         }
-        for(int i=row-1 , j=col-1 ; i>=0  && j>=0 ; i--,j--){
+        for(int i=r-1 , j=c-1 ; i>=0 && j>=0 ; i--,j--){
             if(board[i][j]=='Q') return false;
         }
-        for(int i=row-1 , j=col+1 ; i>=0 && j<N ; i--,j++){
+        for(int i=r-1 , j=c+1 ; i>=0 && j<n; i--,j++){
             if(board[i][j]=='Q') return false;
         }
         return true;
-
     }
-    void solve(vector<string> &board , int row){
-        if(row >= N){
-            result.push_back(board);
+    void solve(int row, int n, vector<string> &temp,vector<vector<string>> &board){
+        if(row>=n){
+            board.push_back(temp);
             return;
         }
-        for(int col = 0 ; col<N ; col++){
-            if(isValid(board,row,col)){
-                board[row][col] = 'Q';
-                solve(board,row+1);
-                board[row][col] = '.';
+        for(int i=0 ; i<n ; i++){
+            if(isValid(temp,row,i,n)){
+                temp[row][i] = 'Q';
+                solve(row+1,n,temp,board);
+                temp[row][i] = '.';
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        N = n;
-        vector<string> board(n,string(n,'.'));
-        solve(board,0);
-        return result;
+        vector<vector<string>> res;
+        vector<string>board(n,string(n,'.'));
+        solve(0,n,board,res);
+        return res;
     }
 };
